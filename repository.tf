950d49d516c728ae09b71d2ec34736d37b28cfce
Jes Struck <mail@jesstruck.dk>
2023-04-20 11:59:53 +0200

    Update repository.tf
    Signed-off-by: Jes Struck <mail@jesstruck.dk>

--- repository.tf
+++ repository.tf
@@ -65,7 +65,7 @@ resource "github_branch_protection" "all" {
 resource "github_repository_tag_protection" "version" {
   for_each = var.repositories
 
-  repository = github_repository.this[each.key].name
+  repository = github_repository.this[each.key].full_name
   pattern    = "v*"
 }
 
