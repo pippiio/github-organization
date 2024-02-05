f539db5197170d6180f5d239ba6ef54aa2062402
Joachim Rorbol <jr@efio.dk>
2024-02-05 14:39:59 +0100

    Changed repo environments variable to hold the entire environment rather than shared

--- actions_environment.tf
+++ actions_environment.tf
@@ -1,21 +1,24 @@
 locals {
-  repo_env = { for entry in flatten([
-    for name, repository in var.repositories : [
-      for environment in repository.environments : [
-        for key, variable in var.environments[environment].variables : {
-          key         = key
-          value       = variable.value
-          sensitive   = variable.sensitive
-          repository  = name
-          environment = try(var.environments[environment].name, environment)
-  }]]]) : "${entry.repository}/${entry.environment}/${entry.key}" => entry }
+  repository_environment = {
+    for entry in flatten([
+      for name, repository in var.repositories : [
+        for environment, envvars in repository.environments : [
+          for key, variable in envvars : {
+            repository  = name
+            environment = environment
+            key         = key
+            value       = variable.value
+            sensitive   = variable.sensitive
+          }
+  ]]]) : "${entry.repository}/${entry.environment}/${entry.key}" => entry }
 }
 
 resource "github_repository_environment" "this" {
-  for_each = { for entry in flatten([for name, repository in var.repositories : [
-    for environment in repository.environments : {
-      repository  = name
-      environment = try(var.environments[environment].name, environment)
+  for_each = {
+    for entry in flatten([for name, repository in var.repositories : [
+      for environment in keys(repository.environments) : {
+        repository  = name
+        environment = environment
   }]]) : "${entry.repository}/${entry.environment}" => entry }
 
   environment = each.value.environment
@@ -23,7 +26,7 @@ resource "github_repository_environment" "this" {
 }
 
 resource "github_actions_environment_secret" "this" {
-  for_each = { for key, envvar in local.repo_env : key => envvar if envvar.sensitive }
+  for_each = { for key, envvar in local.repository_environment : key => envvar if envvar.sensitive }
 
   depends_on = [
     github_repository_environment.this
@@ -36,7 +39,7 @@ resource "github_actions_environment_secret" "this" {
 }
 
 resource "github_actions_environment_variable" "this" {
-  for_each = { for key, envvar in local.repo_env : key => envvar if envvar.sensitive == false }
+  for_each = { for key, envvar in local.repository_environment : key => envvar if envvar.sensitive == false}
 
   depends_on = [
     github_repository_environment.this
