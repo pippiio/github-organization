a7c80ee3dd172a7c4e0ee9a4f05fbe01e1c8f344
Andreas Terkelsen <a.n.terkelsen@gmail.com>
2026-02-13 13:45:26 +0100

    fix: repo-id

--- git_runners.tf
+++ git_runners.tf
@@ -3,7 +3,7 @@ resource "github_actions_runner_group" "this" {
 
   name                    = each.key
   visibility              = each.value.allow_all_repositories ? "all" : "selected"
-  selected_repository_ids = [for repo_name in each.value.repositories : github_repository.this[repo_name].node_id]
+  selected_repository_ids = [for repo_name in each.value.repositories : github_repository.this[repo_name].repo_id]
 }
 
 data "github_actions_organization_registration_token" "this" {
